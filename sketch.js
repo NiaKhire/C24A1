1083d85734ba8bd5d307db5e1d97995b8ed91d9c
NiaKhire <88927810+NiaKhire@users.noreply.github.com>
2021-10-04 15:43:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,86 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world, backgroundImg;
+var canvas, angle, tower, ground, cannon;
+var cannonBall;
+var balls = []; 
+
+var a1;
+a1 = [1,2,3,4,5];
+console.log(a1[3]);
+console.log(a1.length);
+a1.push(6);
+console.log(a1);
+a1.pop();
+console.log(a1);
+
+var a2 =  [10,20 ,[30,40,50], a1];
+console.log(a2);
+
+function preload() {
+  backgroundImg = loadImage("assets/background.gif");
+  towerImage = loadImage("assets/tower.png");
+}
+
+function setup() {
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+  
+  angleMode(DEGREES);
+  angle = 15;
+
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
+  World.add(world, ground);
+
+  tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
+  World.add(world, tower);
+  cannon = new Cannon(180, 110, 130, 100, angle);
+  
+}
+
+function draw() {
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+  Engine.update(engine);
+
+  push();
+  fill("brown");
+  rectMode(CENTER);
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+  pop();
+
+  push();
+  imageMode(CENTER);
+  image(towerImage, tower.position.x, tower.position.y, 160, 310);
+  pop();
+
+  for(var i = 0 ; i<balls.length; i++)  {
+    showCb(balls[i], i);
+  }
+  cannon.display();
+
+  
+
+  
+}
+
+
+function showCb(ball,i){
+ if(ball){
+   ball.display();
+ }
+}
+
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    cannonBall = new CannonBall(cannon.x, cannon.y);
+    balls.push(cannonBall);
+   balls[balls.length-1].shoot();
+  }
+}
